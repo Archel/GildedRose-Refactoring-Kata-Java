d9710a5f91badb4058008f8486b007903d91b54a
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-10 15:08:13 +0100

    refactor: reduce cyclomatic complexity

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -38,22 +38,19 @@ class GildedRose {
                 decreaseQuality(item);
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        increaseQuality(item);
-                    }
-                } else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = 0;
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(item);
-                            }
-                        }
-                    }
-                }
+            if (item.sellIn < 0 && item.name.equals("Aged Brie") && item.quality < 50) {
+                increaseQuality(item);
+            }
+
+            if (item.sellIn < 0 && !item.name.equals("Aged Brie")
+                    && item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0;
+            }
+
+            if (item.sellIn < 0 && !item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+                    && item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                decreaseQuality(item);
             }
         }
     }
