b4039212011937e97539ff6b7690fbb0177476b6
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-10 16:02:17 +0100

    refactor: encapsulation of inizialization of gilded rose

--- src/test/java/com/gildedrose/GildedRoseShould.java
+++ src/test/java/com/gildedrose/GildedRoseShould.java
@@ -29,18 +29,14 @@ public class GildedRoseShould {
 
     @Test(expected = NullPointerException.class)
     public void not_accept_unnamed_items() {
-        Item item = new Item(null, 0, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem(null, 3, 49);
 
         gildedRose.updateQuality();
     }
 
     @Test
     public void not_changes_the_item_name_when_the_name_is_empty() {
-        Item item = new Item("", 0, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("", 0, 0);
 
         gildedRose.updateQuality();
 
@@ -49,9 +45,7 @@ public class GildedRoseShould {
 
     @Test
     public void not_change_the_item_name() {
-        Item item = new Item("Bob", 0, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Bob", 0, 0);
 
         gildedRose.updateQuality();
 
@@ -60,9 +54,7 @@ public class GildedRoseShould {
 
     @Test
     public void not_change_the_quality_when_quality_is_zero() {
-        Item item = new Item("Bob", 0, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Bob", 0, 0);
 
         gildedRose.updateQuality();
 
@@ -71,9 +63,7 @@ public class GildedRoseShould {
 
     @Test
     public void decrease_the_quality_to_zero_when_quality_is_one() {
-        Item item = new Item("Bob", 0, 1);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Bob", 0, 1);
 
         gildedRose.updateQuality();
 
@@ -82,9 +72,7 @@ public class GildedRoseShould {
 
     @Test
     public void decreases_a_quality_by_two_when_quality_is_over_one() {
-        Item item = new Item("Bob", 0, 2);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Bob", 0, 2);
 
         gildedRose.updateQuality();
 
@@ -93,9 +81,7 @@ public class GildedRoseShould {
 
     @Test
     public void decrease_sellIn_by_one_when_sellIn_is_zero() {
-        Item item = new Item("Bob", 0, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Bob", 0, 0);
 
         gildedRose.updateQuality();
 
@@ -104,9 +90,7 @@ public class GildedRoseShould {
 
     @Test
     public void not_change_the_sell_in_for_sulfuras_hand_of_ragnaros() {
-        Item item = new Item("Sulfuras, Hand of Ragnaros", 2, 0);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Sulfuras, Hand of Ragnaros", 2, 0);
 
         gildedRose.updateQuality();
 
@@ -115,9 +99,7 @@ public class GildedRoseShould {
 
     @Test
     public void not_change_the_quality_in_for_sulfuras_hand_of_ragnaros() {
-        Item item = new Item("Sulfuras, Hand of Ragnaros", 0, 54);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Sulfuras, Hand of Ragnaros", 0, 54);
 
         gildedRose.updateQuality();
 
@@ -126,9 +108,7 @@ public class GildedRoseShould {
 
     @Test
     public void increase_by_two_the_quality_of_aged_brie_when_the_quality_is_below_fifty() {
-        Item item = new Item("Aged Brie", 0, -1);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Aged Brie", 0, -1);
 
         gildedRose.updateQuality();
 
@@ -137,9 +117,7 @@ public class GildedRoseShould {
 
     @Test
     public void not_change_the_quality_of_aged_brie_when_the_quality_is_above_fifty() {
-        Item item = new Item("Aged Brie", 0, 50);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Aged Brie", 0, 50);
 
         gildedRose.updateQuality();
 
@@ -148,9 +126,7 @@ public class GildedRoseShould {
 
     @Test
     public void increase_the_quality_by_two_for_backstage_passes_when_sell_in_is_less_than_eleven_and_more_than_six() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Backstage passes to a TAFKAL80ETC concert", 10, 30);
 
         gildedRose.updateQuality();
 
@@ -159,9 +135,7 @@ public class GildedRoseShould {
 
     @Test
     public void increase_the_quality_by_three_for_backstage_passes_when_sell_in_is_less_than_six() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = initializeGildedRoseWithItem("Backstage passes to a TAFKAL80ETC concert", 5, 30);
 
         gildedRose.updateQuality();
 
@@ -170,25 +144,17 @@ public class GildedRoseShould {
 
     @Test
     public void
-    don_t_increase_the_quality_of_aged_brie_over_the_maximum_quality_value() {
-        Item item = new Item("Aged Brie", 0, 49);
-        Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
+    don_t_increase_the_quality_of_backstage_passes_over_the_maximum_quality_value() {
+        GildedRose gildedRose = initializeGildedRoseWithItem("Backstage passes to a TAFKAL80ETC concert", 3, 49);
 
         gildedRose.updateQuality();
 
         assertEquals(50, gildedRose.items[0].quality);
     }
 
-    @Test
-    public void
-    don_t_increase_the_quality_of_backstage_passes_over_the_maximum_quality_value() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 3, 49);
+    private GildedRose initializeGildedRoseWithItem(String name, int sellIn, int quality) {
+        Item item = new Item(name, sellIn, quality);
         Item[] items = new Item[]{item};
-        GildedRose gildedRose = new GildedRose(items);
-
-        gildedRose.updateQuality();
-
-        assertEquals(50, gildedRose.items[0].quality);
+        return new GildedRose(items);
     }
 }
