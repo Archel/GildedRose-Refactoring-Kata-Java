133ca51a810c95f34678e43e980ef0f1fd2bb81a
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-10 18:11:24 +0100

    refactor: transformation of switch to map

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -5,29 +5,33 @@ import com.gildedrose.services.BackstagePassesUpdater;
 import com.gildedrose.services.LegendaryItemsUpdater;
 import com.gildedrose.services.VanillaItemUpdater;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 class GildedRose {
+    private final Map<String, VanillaItemUpdater> updaters;
+    private final VanillaItemUpdater vanillaUpdater = new VanillaItemUpdater();
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
+
+        updaters = Map.ofEntries(
+                entry("Aged Brie", new AgedBrieUpdater()),
+                entry("Sulfuras, Hand of Ragnaros", new LegendaryItemsUpdater()),
+                entry("Backstage passes to a TAFKAL80ETC concert", new BackstagePassesUpdater())
+        );
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
-                    new AgedBrieUpdater().update(item);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    new BackstagePassesUpdater().update(item);
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    new LegendaryItemsUpdater().update(item);
-                    break;
-                default:
-                    new VanillaItemUpdater().update(item);
-                    break;
+            if (item.name == null) {
+                throw new NullPointerException();
             }
+
+            this.updaters.getOrDefault(item.name, vanillaUpdater).update(item);
         }
     }
 }
